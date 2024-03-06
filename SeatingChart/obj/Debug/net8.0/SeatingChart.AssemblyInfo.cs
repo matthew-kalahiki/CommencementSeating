28d461a00f9032a2542a99e82fb356be177a797a
Matthew Kalahiki <kalahikimw@hendrix.edu>
2024-03-06 14:43:44 -0600

    Padding added to seating items

--- SeatingChart/obj/Debug/net8.0/SeatingChart.AssemblyInfo.cs
+++ SeatingChart/obj/Debug/net8.0/SeatingChart.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeatingChart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a3f3dcd2dc17b743523461a9f2057857dc50026")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2214be9db5050e113b61c0cb1fbfbb3306986902")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeatingChart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeatingChart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
